ec5e99bbc96bb5ff19c35e4731255369b2dbae7e
aparsaeeyan <a_parsaeeyan@yahoo.com>
2021-08-30 18:17:45 +0430

    add version tag in csproj file

--- EventViewerLogs/obj/Debug/net5.0/EventViewerLogs.AssemblyInfo.cs
+++ EventViewerLogs/obj/Debug/net5.0/EventViewerLogs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventViewerLogs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventViewerLogs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventViewerLogs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
